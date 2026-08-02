425abf2602c4cfe6ea76c19759ec53aa2c8e2be5
Damir Andrijanić <32528040+hans992@users.noreply.github.com>
2026-08-02 13:51:12 +0200

    Implement real IFC ingestion

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["web-ifc"]
+};
 
 export default nextConfig;
